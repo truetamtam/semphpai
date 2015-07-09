e77fea434028462765895bf5f1a19c406f33d153
truetamtam <truetamtam@yandex.ru>
2015-07-09 11:55:17 +0300

    mysql init.

--- run.sh
+++ run.sh
@@ -2,6 +2,19 @@
 
 # creating mysql db
 DB_NAME=${DB_NAME:-some}
-mysqladmin -uroot create DB_NAME
+
+# wait for mysql server to start (max 30 seconds)
+  timeout=30
+  while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
+  do
+    timeout=$(($timeout - 1))
+    if [ $timeout -eq 0 ]; then
+      echo "Could not connect to mysql server. Aborting..."
+      exit 1
+    fi
+    sleep 1
+  done
+
+/usr/bin/mysqladmin -u root create ${DB_NAME}
 
 supervisord
